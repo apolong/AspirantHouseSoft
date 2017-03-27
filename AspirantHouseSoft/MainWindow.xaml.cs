3dfe615abbad6065558fa202be7951b8f7bb35f7
Jos. Đinh Ngọc Sơn <ad.nspro@gmail.com>
2017-03-27 15:43:00 +0700

    Test

--- AspirantHouseSoft/MainWindow.xaml.cs
+++ AspirantHouseSoft/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace AspirantHouseSoft
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Đã bấm vào button");
+        }
     }
 }
